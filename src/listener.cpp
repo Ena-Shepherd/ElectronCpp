8390f3572d42d1c8f88842ba6d047b643451a504
ENA <62568994+Ena-Shepherd@users.noreply.github.com>
2023-07-25 13:46:19 +0200

    Update and rename mycppcode.cpp to listener.cpp

--- src/listener.cpp
+++ src/listener.cpp
@@ -3,19 +3,6 @@
 
 using json = nlohmann::json;
 
-// Fonction C++ pour traiter les données reçues via la requête AJAX
-json processRequest(const json& requestData) {
-    
-    json responseData;
-
-    if (requestData.contains("key") && requestData["key"] == "ping") {
-        responseData["message"] = "Pong !";
-    }
-    
-    responseData["data"] = requestData;
-    return responseData;
-}
-
 // Point d'entrée pour recevoir les requêtes AJAX
 json myCppFunction(const std::string& requestData) {
     try {
